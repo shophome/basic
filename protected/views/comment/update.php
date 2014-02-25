d40b218de0ef9d4bb904656f6b9d2015e150f85e
He Yin <he.yin@jago-ag.cn>
2014-02-25 17:17:21 +0800

    yii blog demo first step

--- protected/views/comment/update.php
+++ protected/views/comment/update.php
@@ -4,5 +4,7 @@ $this->breadcrumbs=array(
 	'Update Comment #'.$model->id,
 );
 ?>
+
 <h1>Update Comment #<?php echo $model->id; ?></h1>
+
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
