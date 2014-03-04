2e727647eccd23eb4b3b53ad9dd4af0ebd8d1dba
ye.hou <ye.hou@jago-ag.cn>
2014-03-04 10:50:42 +0800

    change   the  style

--- protected/modules/mall/views/paymentMethod/index.php
+++ protected/modules/mall/views/paymentMethod/index.php
@@ -10,8 +10,94 @@ $this->menu=array(
 ?>
 
 <h1>Payment Methods</h1>
+<table class="tab">
+  <tr class="tr-border">
+     <td class="td-border">Payment Method:</td>
+     <td>0</td>
+  </tr>
+  <tr class="tr-border">
+     <td class="td-border">Code:</td>
+     <td></td>
+  </tr>
+  <tr class="tr-border">
+     <td class="td-border">Name:</td>
+     <td>货到付款</td>
+  </tr>
+  <tr class="tr-border">
+    <td class="td-border">Desc:</td>
+    <td> </td>
+  </tr>
+  <tr class="tr-border">
+     <td class="td-border">Config:</td>
+     <td></td>
+  </tr>
+   <tr class="tr-border">
+       <td class="td-border">Enabled</td>
+       <td></td>
+    </tr>
+     <tr class="tr-border">
+         <td class="td-border">Is Code:</td>
+         <td>0</td>
+      </tr>
+</table>
+<table class="tab">
+  <tr class="tr-border">
+     <td class="td-border">Payment Method:</td>
+     <td>1</td>
+  </tr>
+  <tr class="tr-border">
+     <td class="td-border">Code:</td>
+     <td></td>
+  </tr>
+  <tr class="tr-border">
+     <td class="td-border">Name:</td>
+     <td>支付宝</td>
+  </tr>
+  <tr class="tr-border">
+    <td class="td-border">Desc:</td>
+    <td> </td>
+  </tr>
+  <tr class="tr-border">
+     <td class="td-border">Config:</td>
+     <td></td>
+  </tr>
+   <tr class="tr-border">
+       <td class="td-border">Enabled</td>
+       <td></td>
+    </tr>
+     <tr class="tr-border">
+         <td class="td-border">Is Code:</td>
+         <td>0</td>
+      </tr>
+</table>
+<table class="tab">
+  <tr class="tr-border">
+     <td class="td-border">Payment Method:</td>
+     <td>2</td>
+  </tr>
+  <tr class="tr-border">
+     <td class="td-border">Code:</td>
+     <td></td>
+  </tr>
+  <tr class="tr-border">
+     <td class="td-border">Name:</td>
+     <td>财付通</td>
+  </tr>
+  <tr class="tr-border">
+    <td class="td-border">Desc:</td>
+    <td> </td>
+  </tr>
+  <tr class="tr-border">
+     <td class="td-border">Config:</td>
+     <td></td>
+  </tr>
+   <tr class="tr-border">
+       <td class="td-border">Enabled</td>
+       <td></td>
+    </tr>
+     <tr class="tr-border">
+         <td class="td-border">Is Code:</td>
+         <td>0</td>
+      </tr>
+</table>
 
-<?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
-)); ?>
