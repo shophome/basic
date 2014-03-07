cbaf0cef6f9a73d2a4443f7b78c40096fe926da4
Chaozhang Xue <475990465@qq.com>
2014-03-07 14:36:47 +0800

    no message

--- protected/modules/user/views/user/login.php
+++ protected/modules/user/views/user/login.php
@@ -16,21 +16,21 @@
 <?php echo CHtml::beginForm(); ?>
 
 <!--	<p class="note">--><?php //echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?><!--</p>-->
-	
+
 	<?php echo CHtml::errorSummary($model); ?>
-	
+
 	<div class="row">
 		<?php echo CHtml::activeLabelEx($model,'username'); ?>
 		<?php echo CHtml::activeTextField($model,'username') ?>
 	</div>
-	
+
 	<div class="row">
 		<?php echo CHtml::activeLabelEx($model,'password'); ?>
 		<?php echo CHtml::activePasswordField($model,'password') ?>
 	</div>
-	
 
-	
+
+
 	<div class="row rememberMe">
 		<?php echo CHtml::activeCheckBox($model,'rememberMe'); ?>
 		<?php echo CHtml::activeLabelEx($model,'rememberMe'); ?>
@@ -44,7 +44,7 @@
             <?php echo CHtml::link(UserModule::t("Lost Password?"),Yii::app()->getModule('user')->recoveryUrl); ?> | <?php echo CHtml::link(UserModule::t("Register"),Yii::app()->getModule('user')->registrationUrl); ?>
         </p>
     </div>
-	
+
 <?php echo CHtml::endForm(); ?>
 </div><!-- form -->
 
