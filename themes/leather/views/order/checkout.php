af5bfbb17e29718a313402610dbf354ff12bf3b8
Jiadong.Han <Jiadong.Han@jago-ag.cn>
2014-03-12 16:35:03 +0800

    no message

--- themes/leather/views/order/checkout.php
+++ themes/leather/views/order/checkout.php
@@ -69,84 +69,115 @@ Yii::app()->clientScript->registerCoreScript('jquery');
 
 <?php } else { ?>
     <div class="box">
-        <div class="box-title">delivery address</div>
+        <div class="box-title">收货地址：</div>
         <div class="box-content">
             <?php $model = new AddressResult;
             ?>
 
-            <p class="note">Fields with <span class="required">*</span> are required.</p>
+            <div style="width:600px">
 
-            <div class="row">
-                <label for="AddressResult_contact_name" class="required">联系人<span class="required">*</span></label>
-                <input size="45" maxlength="45" name="AddressResult[contact_name]" id="AddressResult_contact_name"
-                       type="text"/>
-            </div>
-            <div class="row" data-url="<?php echo Yii::app()->createUrl('order/getChildAreas'); ?>">
-                <?php
-                $state_data = Area::model()->findAll("grade=:grade",
-                    array(":grade" => 1));
-                $state = CHtml::listData($state_data, "area_id", "name");
-                $s_default = $model->isNewRecord ? '' : $model->state;
-                echo '省&nbsp;' . CHtml::dropDownList('AddressResult[state]', $s_default, $state,
-                        array(
-                            'empty' => '请选择省份',
-                            'ajax' => array(
-                                'type' => 'GET', //request type
-                                'url' => CController::createUrl('dynamiccities'), //url to call
-                                'update' => '#AddressResult_city', //selector to update
-                                'data' => 'js:"AddressResult_state="+jQuery(this).val()',
-                            )));
-                //empty since it will be filled by the other dropdown
-                $c_default = $model->isNewRecord ? '' : $model->city;
-                if (!$model->isNewRecord) {
-                    $city_data = Area::model()->findAll("parent_id=:parent_id",
-                        array(":parent_id" => $model->state));
-                    $city = CHtml::listData($city_data, "id", "name");
-                }
-                $city_update = $model->isNewRecord ? array() : $city;
-                echo '&nbsp;市&nbsp;' . CHtml::dropDownList('AddressResult[city]', $c_default, $city_update,
-                        array(
-                            'empty' => '请选择城市',
-                            'ajax' => array(
-                                'type' => 'GET', //request type
-                                'url' => CController::createUrl('dynamicdistrict'), //url to call
-                                'update' => '#AddressResult_district', //selector to update
-                                'data' => 'js:"AddressResult_city="+jQuery(this).val()',
-                            )));
-                $d_default = $model->isNewRecord ? '' : $model->district;
-                if (!$model->isNewRecord) {
-                    $district_data = Area::model()->findAll("parent_id=:parent_id",
-                        array(":parent_id" => $model->city));
-                    $district = CHtml::listData($district_data, "id", "name");
-                }
-                $district_update = $model->isNewRecord ? array() : $district;
-                echo '&nbsp;区&nbsp;' . CHtml::dropDownList('AddressResult[district]', $d_default, $district_update,
-                        array(
-                            'empty' => '请选择地区',
-                        )
-                    );
-                ?>
-            </div>
-            <div class="row">
-                <label for="AddressResult_zipcode" class="required">邮政编号 <span class="required">*</span></label> <input
-                    size="10" maxlength="45" name="AddressResult[zipcode]" id="AddressResult_zipcode" type="text"/>
-            </div>
-            <div class="row">
-                <label for="AddressResult_address" class="required">详细地址 <span class="required">*</span></label> <input
-                    size="60" maxlength="255" name="AddressResult[address]" id="AddressResult_address" type="text"/>
-            </div>
-            <div class="row">
-                <label for="AddressResult_mobile_phone" class="required">手机 <span class="required">*</span></label>
-                <input size="45" maxlength="45" name="AddressResult[mobile_phone]" id="AddressResult_mobile_phone"
-                       type="text"/></div>
-            <div class="row">
-                <label for="AddressResult_phone">电话</label> <input size="45" maxlength="45" name="AddressResult[phone]"
-                                                                   id="AddressResult_phone" type="text"/></div>
-            <div class="row">
-                <label for="AddressResult_memo">备注</label> <textarea rows="6" cols="50" name="AddressResult[memo]"
-                                                                     id="AddressResult_memo"></textarea></div>
+                <p class="note">带<span class="required">*</span>必须填写！</p>
+                <form role="form" class="form-horizontal"id="addr-form">
+                    <div class="form-group">
+                        <label for="AddressResult_contact_name" class="col-xs-2 control-label">联系人：<span class="required">*</span></label>
+                        <div class="col-xs-10">
+                        <input class="form-control" name="AddressResult[contact_name]" id="AddressResult_contact_name"
+                               type="text"/>
+                        </div>
+                    </div>
+                    <div class="form-group" style="height:40px; overflow:hidden;border:1px solid white">
+                        <lable class="col-xs-2 control-lable" style="width:120px;padding-top:10px;"><b>所在地区：</b><span class="required">*</span></lable>
+                               <div style="width:480px;">
+                            <div class="row" data-url="<?php echo Yii::app()->createUrl('order/getChildAreas'); ?>">
+                                <?php
+                                $state_data = Area::model()->findAll("grade=:grade",
+                                    array(":grade" => 1));
+                                $state = CHtml::listData($state_data, "area_id", "name");
+                                $s_default = $model->isNewRecord ? '' : $model->state;
+                                echo '&nbsp;' . CHtml::dropDownList('AddressResult[state]', $s_default, $state,
+                                        array(
+                                            'empty' => '请选择省份',
+                                            'ajax' => array(
+                                                'type' => 'GET', //request type
+                                                'url' => CController::createUrl('dynamiccities'), //url to call
+                                                'update' => '#AddressResult_city', //selector to update
+                                                'data' => 'js:"AddressResult_state="+jQuery(this).val()',
+                                            )));
+                                //empty since it will be filled by the other dropdown
+                                $c_default = $model->isNewRecord ? '' : $model->city;
+                                if (!$model->isNewRecord) {
+                                    $city_data = Area::model()->findAll("parent_id=:parent_id",
+                                        array(":parent_id" => $model->state));
+                                    $city = CHtml::listData($city_data, "id", "name");
+                                }
+                                $city_update = $model->isNewRecord ? array() : $city;
+                                echo '&nbsp;&nbsp;' . CHtml::dropDownList('AddressResult[city]', $c_default, $city_update,
+                                        array(
+                                            'empty' => '请选择城市',
+                                            'ajax' => array(
+                                                'type' => 'GET', //request type
+                                                'url' => CController::createUrl('dynamicdistrict'), //url to call
+                                                'update' => '#AddressResult_district', //selector to update
+                                                'data' => 'js:"AddressResult_city="+jQuery(this).val()',
+                                            )));
+                                $d_default = $model->isNewRecord ? '' : $model->district;
+                                if (!$model->isNewRecord) {
+                                    $district_data = Area::model()->findAll("parent_id=:parent_id",
+                                        array(":parent_id" => $model->city));
+                                    $district = CHtml::listData($district_data, "id", "name");
+                                }
+                                $district_update = $model->isNewRecord ? array() : $district;
+                                echo '&nbsp;&nbsp;' . CHtml::dropDownList('AddressResult[district]', $d_default, $district_update,
+                                        array(
+                                            'empty' => '请选择地区',
+                                        )
+                                    );
+                                ?>
+                                </div>
+                                </div>
+                               </div>
 
 
+
+                    <div class="form-group" style="height:35px; border:1px solid white">
+                        <label for="AddressResult_zipcode" class=" col-xs-2 control-label">邮政编号 <span class="required">*</span></label>
+                         <div class="col-xs-10">
+                         <input
+                            name="AddressResult[zipcode]" id="AddressResult_zipcode"class="form-control"  type="text"/>
+                         </div>
+                    </div>
+                    <div class="form-group" style="height:35px; border:1px solid white">
+                        <label for="AddressResult_address" class="col-xs-2 control-label">详细地址 <span class="required">*</span></label>
+                         <div class="col-xs-10">
+                         <input
+                            name="AddressResult[address]" id="AddressResult_address" type="text" class="form-control" />
+                            </div>
+                    </div>
+
+                    <div class="form-group" style="height:35px;border:1px solid white">
+
+                        <label for="AddressResult_mobile_phone" class="col-xs-2 control-label">手机 <span class="required">*</span></label>
+                        <div class="col-xs-10">
+                        <input  name="AddressResult[mobile_phone]" id="AddressResult_mobile_phone" class="form-control"
+                               type="text"/>
+                               </div>
+                               </div>
+                    <div class="form-group" style="height:35px;border:1px solid white">
+                        <label for="AddressResult_phone" class="col-xs-2 control-label">电话</label>
+                         <div class=" col-xs-10">
+                         <input   name="AddressResult[phone]" class="form-control"
+                                                                           id="AddressResult_phone" type="text"/></div>
+                          </div>
+                    <div class="form-group" >
+                        <label for="AddressResult_memo" class="col-xs-2 control-label">备注</label>
+                        <div class=" col-xs-10">
+                        <textarea rows="6" cols="50" name="AddressResult[memo]" class="form-control"
+                                                                             id="AddressResult_memo"></textarea></div>
+                         </div>
+
+
+                </div>
+         </form>
         </div>
     </div>
 
@@ -169,15 +200,14 @@ Yii::app()->clientScript->registerCoreScript('jquery');
     <div class="box">
         <div class="box-title container_24">商品列表</div>
         <div class="box-content cart container_24">
-            <table width="90%" border="1" cellspacing="1" cellpadding="0"
-                   id="list-div-box" class=" table-bordered table table-striped table-hover">
+            <table id="list-div-box" class="table">
                 <tr style="background:#F3F3F3;">
-                    <th width="15%">图片</th>
-                    <th width="15%">名称</th>
-                    <th width="15%">属性</th>
-                    <th width="15%">价格</th>
-                    <th width="15%">数量</th>
-                    <th width="15%">小计</th>
+                    <th class="col-xs-3">图片</th>
+                    <th class="col-xs-3">名称</th>
+                    <th class="col-xs-3">属性</th>
+                    <th class="col-xs-1">价格</th>
+                    <th class="col-xs-1">数量</th>
+                    <th class="col-xs-1">小计</th>
                 </tr>
                 <?php
                 if (isset($item)) {
@@ -235,14 +265,15 @@ Yii::app()->clientScript->registerCoreScript('jquery');
                     <textarea id="memo" name="memo" placeholder="选填，可以告诉卖家您对商品的特殊要求，如：颜色、尺码等" rows="5"></textarea>
             </div>
         </div>
+
+            <button class="btn btn-danger pull-right" style="line-height:20px;margin-right:150px;margin-bottom:30px;" href="">确认订单</button>
+
+
+
+
     </div>
 
-    <div class="order-confirm" style="margin: 0 auto;width: 1180px">
-        <span
-            style="float: right"><?php echo CHtml::link('确认订单', '#', array('id' => 'confirmOrder', 'class' => 'btn')) ?></span>
 
-    </div>
-    <div style="clear: both;margin-bottom: 10px"></div>
 <?php echo CHtml::endForm() ?>
 <script type="text/javascript">
     $('#AddressResult_state, #AddressResult_city').change(function() {
